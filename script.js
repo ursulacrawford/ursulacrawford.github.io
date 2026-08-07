a203f9ed321132aebefd70a284a13e357d222464
ursulacrawford <ursulacrawford1@gmail.com>
2026-08-07 18:41:37 +1200

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,53 @@ document.body.style.opacity="1";
 
 
 });
+const images = [
+
+"images/look1.jpg",
+
+"images/look2.jpg",
+
+"images/look3.jpg",
+
+"images/look4.jpg",
+
+"images/look5.jpg"
+
+];
+
+
+let current = 0;
+
+
+const heroImage = document.getElementById("changing-image");
+
+
+setInterval(()=>{
+
+
+heroImage.style.opacity = 0;
+
+
+setTimeout(()=>{
+
+
+current++;
+
+if(current >= images.length){
+
+current = 0;
+
+}
+
+
+heroImage.src = images[current];
+
+
+heroImage.style.opacity = 1;
+
+
+},1500);
+
+
+
+},6000);
