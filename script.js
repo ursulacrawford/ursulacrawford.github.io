b3c7de7417f24ea6777cdcb69d7b060fb045a3d2
ursulacrawford <ursulacrawford1@gmail.com>
2026-08-07 18:21:32 +1200

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+document.body.style.opacity="0";
+
+setTimeout(()=>{
+
+document.body.style.transition="opacity 1s";
+
+document.body.style.opacity="1";
+
+},100);
+
+
+});
